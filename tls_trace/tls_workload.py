837350a2848a8afc2fdcfa01edaebb0629f86064
Ethan Van Der Heijden <ethanvanderheijden@gmail.com>
2025-09-08 21:14:16 -0400

    Add workload driver for TLS trace.

---
+++ tls_trace/tls_workload.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+
+import asyncio
+import pandas as pd
+import random
+import time
+import ssl
+
+data = pd.read_csv("output/combined.csv")
+trace_id = random.choice(data["trace"].unique())
+traces = data[data["trace"] == trace_id]
+
+ssl_context = ssl.create_default_context()
+ssl_context.check_hostname = False
+ssl_context.verify_mode = ssl.CERT_NONE
+ssl_context.maximum_version = ssl.TLSVersion.TLSv1_2
+ssl_context.minimum_version = ssl.TLSVersion.TLSv1_2
+
+_tasks = set()
+
+
+def spawn_task(coro):
+    task = asyncio.create_task(coro)
+    _tasks.add(task)
+    task.add_done_callback(_tasks.discard)
+
+
+async def tls_connect(domain):
+    start = time.time()
+    print("Visiting", domain)
+    try:
+        _, writer = await asyncio.open_connection(domain, 443)
+        await writer.start_tls(
+            ssl_context, ssl_handshake_timeout=5, ssl_shutdown_timeout=5, server_hostname=domain
+        )
+        writer.close()
+        await writer.wait_closed()
+        end = time.time()
+        print(f"Connected to {domain} in {(end - start) * 1000} ms")
+    except Exception as e:
+        print(f"Error connecting to {domain}: {e}")
+
+
+async def main():
+    start = time.time()
+    for _, row in traces.iterrows():
+        diff = row["timestamp"] - (time.time() - start)
+        if diff > 0:
+            print("Sleeping for", diff)
+            await asyncio.sleep(diff)
+        spawn_task(tls_connect(row["url"]))
+
+
+asyncio.run(main())
